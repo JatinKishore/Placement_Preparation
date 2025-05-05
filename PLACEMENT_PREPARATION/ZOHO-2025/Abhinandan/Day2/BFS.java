a32f4680a1708479b8cd0b89397bf65d376c5887
thisisabhinandan <abhinandanr@icloud.com>
2025-05-06 02:08:14 +0530

    Day 2

--- PLACEMENT_PREPARATION/ZOHO-2025/Abhinandan/Day2/BFS.java
+++ PLACEMENT_PREPARATION/ZOHO-2025/Abhinandan/Day2/BFS.java
@@ -1,4 +1,57 @@
 package Day2;
 
-public class BFS {
+
+import java.util.*;
+
+public class BFS{
+    static List<List<Integer>> adjList=new ArrayList<>();
+    static List<Integer> bfsList=new ArrayList<>();
+    public static void CreateGraph(int u, int v)
+    {
+        adjList.get(u).add(v);
+        adjList.get(v).add(u);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number of Nodes");
+        int nodes=sc.nextInt();
+        System.out.println("Enter Number of Edges");
+        int edges=sc.nextInt();
+        for(int i=0;i<nodes;i++)
+        {
+            adjList.add(new ArrayList<>());
+        }
+        System.out.println("Enter the Connections");
+        for(int i=0;i<edges;i++)
+        {
+            int u=sc.nextInt();
+            int v=sc.nextInt();
+            CreateGraph(u,v);
+        }
+        boolean [] vis=new boolean [nodes];
+        BFSTraversal(adjList,vis,0);
+        for (int i=0;i<nodes;i++)
+        {
+            System.out.print(bfsList.get(i)+" ");
+        }
+    }
+
+    private static void BFSTraversal(List<List<Integer>> adjList, boolean[] vis, int node) {
+        Queue<Integer> bfsQueue=new LinkedList<>();
+        bfsQueue.add(node);
+        vis[node]=true;
+        while(!bfsQueue.isEmpty())
+        {
+            int curr=bfsQueue.poll();
+            bfsList.add(curr);
+            for(int neighbour:adjList.get(curr))
+            {
+                if(!vis[neighbour])
+                {
+                    vis[neighbour]=true;
+                    bfsQueue.add(neighbour);
+                }
+            }
+        }
+    }
 }
