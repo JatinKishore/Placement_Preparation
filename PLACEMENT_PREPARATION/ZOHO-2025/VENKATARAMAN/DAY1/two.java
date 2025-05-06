2065cc6e2ea81b1c2ed60dadf8e4191ae3beabe9
Venkataraman P <83152277+Alphacode14052004@users.noreply.github.com>
2025-05-06 12:02:58 +0530

    Create two.java

---
+++ PLACEMENT_PREPARATION/ZOHO-2025/VENKATARAMAN/DAY1/two.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class PairSumSimple {
+
+    public static List<int[]> findPairs(int[] arr, int target) {
+        List<int[]> result = new ArrayList<>();
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> used = new HashSet<>();
+
+        for (int num : arr) {
+            int comp = target - num;
+            if (seen.contains(comp) && !used.contains(num) && !used.contains(comp)) {
+                // add the pair in ascending order
+                result.add(new int[]{Math.min(num, comp), Math.max(num, comp)});
+                used.add(num);
+                used.add(comp);
+            }
+            seen.add(num);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 2, 4, 3};
+        int target = 5;
+        List<int[]> pairs = findPairs(nums, target);
+        for (int[] p : pairs) {
+            System.out.println(p[0] + ", " + p[1]);
+        }
+    }
+}
