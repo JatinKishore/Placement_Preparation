f77850f0298d393c317b995360023964153f4064
Nilla14 <nilla14r@gmail.com>
2025-05-15 17:24:25 +0530

    Update _01_Count1s.java

--- PLACEMENT_PREPARATION/ZOHO-2025/Nilla/Day 6 /_01_Count1s.java
+++ PLACEMENT_PREPARATION/ZOHO-2025/Nilla/Day 6 /_01_Count1s.java
@@ -1,24 +1,18 @@
 public class Count1s {
-    public static int countNoConsecutiveOnes(int n) {
-        int[][] dp = new int[n+1][2];
-        dp[1][0] = 1;  
-        dp[1][1] = 1;  
-
+      public static int countStringsWithConsecutiveOnes(int n) {
+        if (n == 0) return 0;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 2; 
         for (int i = 2; i <= n; i++) {
-            dp[i][0] = dp[i-1][0] + dp[i-1][1];
-            dp[i][1] = dp[i-1][0];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-
-        return dp[n][0] + dp[n][1];
-    }
-
-    public static int countWithConsecutiveOnes(int n) {
         int total = (int) Math.pow(2, n);
-        return total - countNoConsecutiveOnes(n);
+        return total - dp[n];
     }
 
     public static void main(String[] args) {
-        int n = 3;
-        System.out.println("Strings of length " + n + " with consecutive 1’s: " + countWithConsecutiveOnes(n));
+        int n = 5;
+        System.out.println("Count: " + countStringsWithConsecutiveOnes(n));
     }
 }
