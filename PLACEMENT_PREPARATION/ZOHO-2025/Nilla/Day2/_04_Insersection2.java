125b5716b065a952221ccd8df17cc2d64ecbcf20
Nilla14 <nilla14r@gmail.com>
2025-05-15 11:11:38 +0530

    Update _04_Insersection2.java

--- PLACEMENT_PREPARATION/ZOHO-2025/Nilla/Day2/_04_Insersection2.java
+++ PLACEMENT_PREPARATION/ZOHO-2025/Nilla/Day2/_04_Insersection2.java
@@ -27,6 +27,6 @@ class Intersection2 {
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 2, 1};
         int[] nums2 = {2, 2};
-        System.out.println("Output: " + Arrays.toString(Intersection2(nums1, nums2))); 
+        System.out.println("Output: " + Arrays.toString(intersect(nums1, nums2))); 
     }
 }
