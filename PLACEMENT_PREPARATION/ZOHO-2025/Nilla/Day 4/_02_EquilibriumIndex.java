b383ce7d5b6a8e10fce55be48d7dd305fa3695fa
Nilla14 <nilla14r@gmail.com>
2025-05-15 15:22:35 +0530

    Create _02_EquilibriumIndex.java

---
+++ PLACEMENT_PREPARATION/ZOHO-2025/Nilla/Day 4/_02_EquilibriumIndex.java
@@ -0,0 +1,27 @@
+public class EquilibriumIndex {
+   public static int findEquilibriumIndex(int[] arr) {
+        int totalSum = 0;
+        for (int num : arr) {
+            totalSum += num;
+        }
+        int leftSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            totalSum -= arr[i]; 
+            if (leftSum == totalSum) {
+                return i; 
+            }
+            leftSum += arr[i]; 
+        }
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { -7, 1, 5, 2, -4, 3, 0 };
+        int index = findEquilibriumIndex(arr);
+        if (index != -1) {
+            System.out.println("Equilibrium index is: " + index);
+        } else {
+            System.out.println("No equilibrium index found.");
+        }
+    }
+}
